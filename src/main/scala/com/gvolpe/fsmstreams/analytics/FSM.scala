9cded4a1e1d83656eb01dcca10d0aba245f11331
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-29 11:34:05 +0100

    add identity fsm

--- src/main/scala/com/gvolpe/fsmstreams/analytics/FSM.scala
+++ src/main/scala/com/gvolpe/fsmstreams/analytics/FSM.scala
@@ -1,3 +1,9 @@
 package com.gvolpe.fsmstreams.analytics
 
+import cats.Id
+
 case class FSM[F[_], S, I, O](run: (S, I) => F[(S, O)])
+
+object FSM {
+  def identity[S, I, O](run: (S, I) => Id[(S, O)]) = FSM(run)
+}
